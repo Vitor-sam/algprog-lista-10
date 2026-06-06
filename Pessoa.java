b313def96bba5a82fe8f79787015bef2bae9378b
Vitor-sam <vitor2008samuca@gmail.com>
2026-06-05 21:12:40 -0300

    Add files via upload

---
+++ Pessoa.java
@@ -0,0 +1,38 @@
+
+public class Pessoa {
+
+    String nome;
+    int idade;
+    double altura;
+    double peso;
+
+    public Pessoa(String nome, int idade, double altura, double peso) {
+        this.nome = nome;
+        this.idade = idade;
+        this.altura = altura;
+        this.peso = peso;
+    }
+
+    public void falar() {
+        System.out.println(nome + " está falando.");
+    }
+
+    public void andar() {
+        System.out.println(nome + " está andando.");
+    }
+
+    public void comer() {
+        System.out.println(nome + " está comendo.");
+    }
+
+    public void dormir() {
+        System.out.println(nome + " está dormindo.");
+    }
+
+    public void mostrarDados() {
+        System.out.println("Nome: " + nome);
+        System.out.println("Idade: " + idade);
+        System.out.println("Altura: " + altura);
+        System.out.println("Peso: " + peso);
+    }
+}
