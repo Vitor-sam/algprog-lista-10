3f12da4fcacebdde4d43ce828cfd1c4d089aedf9
Vitor-sam <vitor2008samuca@gmail.com>
2026-06-05 21:15:12 -0300

    Add files via upload

---
+++ Animal.java
@@ -0,0 +1,7 @@
+public class Animal 
+{ String especie; String cor; int idade; public Animal(String especie, String cor, int idade)
+     
+    { this.especie = especie; this.cor = cor; this.idade = idade; }
+    public void correr()
+     {} public void dormir() 
+     {} public void comer() {} }
